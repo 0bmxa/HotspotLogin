e2d1e8c915b70fe3130626019864d374234c63f9
0bmxa <0bmxa@users.noreply.github.com>
2020-08-31 10:51:37 +0200

    Disable unused includes

--- HotspotLogin/Networking/hotspot_login-Bridging-Header.h
+++ HotspotLogin/Networking/hotspot_login-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/nameser.h>
-#include <resolv.h>
+//#include <sys/types.h>
+//#include <netinet/in.h>
+//#include <arpa/nameser.h>
+//#include <resolv.h>
